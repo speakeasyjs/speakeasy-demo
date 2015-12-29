a1d9890f30bb4e0e264c97d496b26f9e99525c83
Mark Bao <mark@markbao.com>
2015-12-29 13:49:39 -0800

    Updated with better counter and new qrcode

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var speakeasy = require('speakeasy')
+var qr = require('qr-image');
 var express = require('express');
 var app = express();
 
@@ -12,7 +13,8 @@ app.get('/', function (req, res) {
   locals.secret = speakeasy.generate_key({length: 20})
 
   // Get QR code URL
-  locals.qrURL = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=" + encodeURIComponent('otpauth://totp/SecretKey?secret=' + encodeURIComponent(locals.secret.base32))
+  locals.qrURL = 'otpauth://totp/SecretKey?secret=' + encodeURIComponent(locals.secret.base32);
+  locals.qrPath = '/qrcode?qrurl=' + encodeURIComponent(locals.qrURL);
 
   // Get initial token
   locals.token = getToken(locals.secret.base32)
@@ -24,10 +26,16 @@ app.get('/token', function (req, res) {
   res.send(getToken(req.query.secret));
 });
 
+app.get('/qrcode', function(req, res) {
+  var code = qr.image(req.query.qrurl, { type: 'png' });
+  res.type('png');
+  code.pipe(res);
+});
+
 function getToken(secret) {
   return speakeasy.time({key: secret, encoding: 'base32'})
 }
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8081
     , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 app.listen(port, ip);
