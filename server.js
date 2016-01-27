8d970f4ef88227c42e77f6a56819a2c982b4734d
Mark Bao <mark@markbao.com>
2016-01-27 02:16:34 -0500

    [change] Update for Speakeasy 2.0.0

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ app.get('/', function (req, res) {
   var locals = {}
 
   // Generate a secret
-  locals.secret = speakeasy.generate_key({length: 20})
+  locals.secret = speakeasy.generateSecret({length: 20})
 
   // Get QR code URL
-  locals.qrURL = 'otpauth://totp/SecretKey?secret=' + encodeURIComponent(locals.secret.base32);
-  locals.qrPath = '/qrcode?qrurl=' + encodeURIComponent(locals.qrURL);
+  locals.qrPath = '/qrcode?qrurl=' + encodeURIComponent(locals.secret.otpauth_url);
 
   // Get initial token
   locals.token = getToken(locals.secret.base32)
@@ -26,6 +25,14 @@ app.get('/token', function (req, res) {
   res.send(getToken(req.query.secret));
 });
 
+app.get('/verify', function (req, res) {
+  if (verifyToken(req.query.secret, req.query.token)) {
+    res.send('Verified');
+  } else {
+    res.send('Failed to verify');
+  }
+});
+
 app.get('/qrcode', function(req, res) {
   var code = qr.image(req.query.qrurl, { type: 'png' });
   res.type('png');
@@ -33,7 +40,11 @@ app.get('/qrcode', function(req, res) {
 });
 
 function getToken(secret) {
-  return speakeasy.time({key: secret, encoding: 'base32'})
+  return speakeasy.time({secret: secret, encoding: 'base32'})
+}
+
+function verifyToken(secret, token) {
+  return speakeasy.time.verify({secret: secret, encoding: 'base32', token: token})
 }
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8081
